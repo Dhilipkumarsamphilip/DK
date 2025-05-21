e1d34e838b92eb0f684c445486df5fbe77003c68
DHILIPKUMARSAMPHILIP <94403996+Dhilipkumarsamphilip@users.noreply.github.com>
2025-05-21 08:33:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-084568db4383264d4"  # Specify an appropriate AMI ID
+    ami           = "ami-084568db4383264d4"
     instance_type = "t2.micro"
 }
