a67f9f90a9c7570034c5ed93eec1f0d7ee76a6b4
DHILIPKUMARSAMPHILIP <94403996+Dhilipkumarsamphilip@users.noreply.github.com>
2025-05-22 11:26:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "eu-north-1"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-084568db4383264d4"
+    ami           = "ami-00992c69aee3b7f50"
     instance_type = "t2.micro"
 }
