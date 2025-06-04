f372b239d493b44b558f76ae3f97620c6e172c1a
DHILIPKUMARSAMPHILIP <94403996+Dhilipkumarsamphilip@users.noreply.github.com>
2025-06-04 16:46:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,27 @@
+variable "ami_value" {
+    default = ""
+    description = "value for the ami"
+    type = string   
+}
 
+variable "instance_type" {
+    default = ""
+    description = "value for the instance type"
+    type = string   
+}
+
+variable "subnet_id" {
+    default = ""
+    description = "value for the subnet id"
+    type = string   
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami           = "var.ami_value"
+    instance_type = "var.instance_type"
+    subnet_id = "var.subnet_id"
+}
