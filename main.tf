bf390cccec52d28e605a97d07d103a50cf9842cc
DHILIPKUMARSAMPHILIP <94403996+Dhilipkumarsamphilip@users.noreply.github.com>
2025-06-04 15:31:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,27 @@
+variable "ami_value" {
+    default = ""
+    description = "value for the ami"
+    type = string   
+}
+
+variable "instance_type" {
+    default = ""
+    description = "value for the instance type"
+    type = string   
+}
+
+variable "subnet_id" {
+    default = ""
+    description = "value for the subnet id"
+    type = string   
+}
+
 provider "aws" {
     region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-084568db4383264d4"
-    instance_type = "t2.micro"
+    ami           = "var.ami_value"
+    instance_type = "var.instance_type"
+    subnet_id = "var.subnet_id"
 }
