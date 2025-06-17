939a0b76efad1c0bd44f4fb5f30f63210ea9adb7
DHILIPKUMARSAMPHILIP <94403996+Dhilipkumarsamphilip@users.noreply.github.com>
2025-06-17 22:34:09 +0530

    Update server.py

--- commontask/server.py
+++ commontask/server.py
@@ -1,21 +1,102 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+provider "aws" {
+  region = "us-east-1"
+}
 
-class SimpleHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(b"Hello, Chris Paul Matthew!")
-        else:
-            self.send_response(404)
-            self.end_headers()
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
 
-def run(server_class=HTTPServer, handler_class=SimpleHandler):
-    server_address = ('0.0.0.0', 80)
-    httpd = server_class(server_address, handler_class)
-    print("Starting HTTP server on port 80...")
-    httpd.serve_forever()
+resource "aws_key_pair" "example" {
+  key_name = "terra_task"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
 
-if __name__ == "__main__":
-    run()
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "RT1" {
+  subnet_id = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "asg" {
+  name = "web_security"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from vpc"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress{
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "asg"
+  }
+}
+
+resource "aws_instance" "server" {
+  ami = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.asg.id]
+  subnet_id = aws_subnet.sub1.id
+  associate_public_ip_address = true
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+  }
+
+  provisioner "file" {
+    source = "server.py"
+    destination = "/home/ubuntu/server.py"
+  }
+
+  provisioner "remote-exec" {
+  inline = [ 
+    "sudo apt-get update -y",
+    "sudo apt-get install -y python3",
+    "cd /home/ubuntu",
+    "sudo nohup python3 server.py > server.log 2>&1 &"
+    ]
+  }
+}
